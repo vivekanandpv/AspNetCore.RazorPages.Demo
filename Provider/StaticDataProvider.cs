93e339267f9a846478e438d857b0023a9ee48bbf
vivek <vivek@athenaeum.in>
2021-05-07 07:28:52 +0530

    done

---
+++ Provider/StaticDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.RazorPages.Demo.Provider
+{
+    public class StaticDataProvider
+    {
+        public Task<IEnumerable<string>> GetCitiesAsync()
+        {
+            var cities = new List<string> { "Mumbai", "Bengaluru", "Chennai" };
+
+            return Task.Run(() =>
+            {
+                return cities.AsEnumerable();
+            });
+        }
+    }
+}
